3bc705fb444add86018e2ff4f0ae5c0f9add077b
Farley Knight <farleyknight@gmail.com>
2014-06-12 15:45:13 -0400

    Add variables

--- Parser.hs
+++ Parser.hs
@@ -1,5 +1,6 @@
 module Parser where
 
+import Data.Char (isAlpha, toLower, toUpper, isSpace, digitToInt)
 import Data.Text hiding (unlines)
 import Data.Functor
 import Control.Monad
@@ -12,36 +13,55 @@ import Text.ParserCombinators.Parsec.Language
 import Test.Hspec
 import Test.QuickCheck
 
-data BinOpExpr = Unit Integer
+data BinOpExpr = Natural Integer
+               | Variable String
                | Negative BinOpExpr
                | Positive BinOpExpr
                | Product BinOpExpr BinOpExpr
                | Divide BinOpExpr BinOpExpr
                | Difference BinOpExpr BinOpExpr
-               | Sum BinOpExpr BinOpExpr deriving (Show, Eq)
+               | Sum BinOpExpr BinOpExpr
+               | Assign BinOpExpr BinOpExpr
+               deriving (Show, Eq)
+
+(<*>) :: Monad m => m t1 -> m t2 -> m (t1, t2)
+a <*> b = do
+  x <- a
+  y <- b
+  return (x, y)
+
 
 lexer :: P.TokenParser ()
 lexer = P.makeTokenParser
         (emptyDef
         {
-          reservedOpNames = ["*","/","+","-"]
+          reservedOpNames = ["*","/","+","-"],
+          identStart      = letter <|> char '_',
+          identLetter     = alphaNum <|> oneOf "_'"
         })
 
-natural    = P.natural lexer
-parens     = P.parens lexer
-reservedOp = P.reservedOp lexer
+natural      = P.natural lexer
+parens       = P.parens lexer
+reservedOp   = P.reservedOp lexer
+name         = P.lexeme lexer name'
+  where
+    name' = (uncurry (:)) <$> (letter <*> (many alphaNum))
 
 binaryOp = buildExpressionParser table term
 
-term = binOpNatural <|>
-       parens binaryOp
+term = (parens binaryOp)
+       <|> atom
 
-binOpNatural = Unit <$> natural
+atom = nat <|> var
+  where
+    nat = Natural <$> natural
+    var = Variable <$> name
 
 table = [
   [prefix "-" Negative,          prefix "+" id],
   [binary "*" Product AssocLeft, binary "/" Divide AssocLeft],
-  [binary "+" Sum AssocLeft,     binary "-" Difference AssocLeft]
+  [binary "+" Sum AssocLeft,     binary "-" Difference AssocLeft],
+  [binary "=" Assign AssocRight]
   ]
 
 binary  name fun assoc = Infix (do{ reservedOp name; return fun }) assoc
@@ -50,7 +70,6 @@ postfix name fun       = Postfix (do{ reservedOp name; return fun })
 
 parseBinaryOp = parse binaryOp ""
 
--- TODO: Add some tests!
 should :: Example a => String -> a -> Spec
 should x = it ("should " ++ x)
 
